dba2d12146db6a7dde86d3d6809f107eb4fb6e22
K-IoU <mencraft10@gmail.com>
2023-09-15 16:28:52 +0200

    Finish Crypto Zombie

--- src/zombiefactory.sol
+++ src/zombiefactory.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract ZombieFactory{
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable{
 
     event NewZombie(uint zombieId,string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint64 cooldownTime = 1 days;
 
     struct Zombie{
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -18,8 +25,8 @@ contract ZombieFactory{
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) public ownerZombieCount;
 
-    function _createZombie(string memory _name, uint _dna) private{
-        zombies.push(Zombie(_name,_dna));
+    function _createZombie(string memory _name, uint _dna) internal {
+        zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime), 0, 0));
         uint id = (zombies.length)-1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
@@ -37,7 +44,4 @@ contract ZombieFactory{
         _createZombie(_name,randDna);
     }
 
-}
-
-contract ZombieFeeding is ZombieFactory{   
 }
